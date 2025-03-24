6ee1c98cbb677c9a3fd358f4cd465fffa403a070
Anakama <109427414+AnakamaTH@users.noreply.github.com>
2025-03-24 19:29:25 +0300

    alr better

--- build.bat
+++ build.bat
@@ -1,19 +1,58 @@
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJmcXs=
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWFiK9UkxJRo0
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDpaTQ+DMWqGIrof/eX+4f6UnmoUQMoqerPI1bqHI61Cp0LtYZ441DdXmc8CCQhLM0GXbBo9pWoMs3yAVw==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 
+title %random%
+
 echo skibidi brainrot toilet goofy soo goated
+title %random%
 echo hamburger
+title %random%
 echo ninja low taper fade
+title %random%
 echo bro got brainrot
+title %random%
 echo what the sigma
-
+title %random%
 pause 2 > nul
-
+title %random%
 
 asm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- sonic1.asm, s1built.bin, s1built.sym, sonic1.lst
 convsym sonic1.lst s1built.bin -input asm68k_lst -inopt "/localSign=@ /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+" -a
 fixheadr.exe s1built.bin
-
+title %random%
 cls
 echo see ya
+title %random%
 
 pause
