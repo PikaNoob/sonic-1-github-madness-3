597ffb056128a41cf2012e7362e01d4abe619141
MattM4nia <77948135+mattm4nia@users.noreply.github.com>
2025-03-24 16:56:37 -0500

    ballhog sprites

--- _maps/obj1E.asm
+++ _maps/obj1E.asm
@@ -1,23 +1,26 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - Ball Hog enemy (SBZ)
-; ---------------------------------------------------------------------------
-		dc.w byte_8E6E-Map_obj1E, byte_8E79-Map_obj1E
-		dc.w byte_8E84-Map_obj1E, byte_8E8F-Map_obj1E
-		dc.w byte_8E9A-Map_obj1E, byte_8EA0-Map_obj1E
-byte_8E6E:	dc.b 2
-		dc.b $EF, 9, 0,	0, $F4
-		dc.b $FF, $A, 0, 6, $F4
-byte_8E79:	dc.b 2
-		dc.b $EF, 9, 0,	0, $F4
-		dc.b $FF, $A, 0, $F, $F4
-byte_8E84:	dc.b 2
-		dc.b $F4, 9, 0,	0, $F4
-		dc.b 4,	9, 0, $18, $F4
-byte_8E8F:	dc.b 2
-		dc.b $E4, 9, 0,	0, $F4
-		dc.b $F4, $A, 0, $1E, $F4
-byte_8E9A:	dc.b 1
-		dc.b $F8, 5, 0,	$27, $F8
-byte_8EA0:	dc.b 1
-		dc.b $F8, 5, 0,	$2B, $F8
-		even
+Map_a7ad:
+Map_a7ad_0: 	dc.w Map_a7ad_C-Map_a7ad
+Map_a7ad_2: 	dc.w Map_a7ad_17-Map_a7ad
+Map_a7ad_4: 	dc.w Map_a7ad_22-Map_a7ad
+Map_a7ad_6: 	dc.w Map_a7ad_2D-Map_a7ad
+Map_a7ad_8: 	dc.w Map_a7ad_38-Map_a7ad
+Map_a7ad_A: 	dc.w Map_a7ad_43-Map_a7ad
+Map_a7ad_C: 	dc.b $2
+	dc.b $EF, $9, $0, $0, $F4
+	dc.b $FF, $A, $0, $6, $F4
+Map_a7ad_17: 	dc.b $2
+	dc.b $EF, $9, $0, $0, $F4
+	dc.b $FF, $A, $0, $6, $F4
+Map_a7ad_22: 	dc.b $2
+	dc.b $F4, $9, $0, $0, $F4
+	dc.b $4, $9, $0, $18, $F4
+Map_a7ad_2D: 	dc.b $2
+	dc.b $E4, $9, $0, $0, $F4
+	dc.b $F4, $A, $0, $1E, $F4
+Map_a7ad_38: 	dc.b $2
+	dc.b $F8, $5, $0, $27, $F8
+	dc.b $E8, $5, $0, $F, $F8
+Map_a7ad_43: 	dc.b $2
+	dc.b $F8, $5, $0, $2B, $F8
+	dc.b $E8, $5, $0, $13, $F8
+	even
