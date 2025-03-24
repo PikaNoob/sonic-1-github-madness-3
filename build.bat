15a1ed78aedfb2dadcc6733ff47370610d43c1dc
Anakama <109427414+AnakamaTH@users.noreply.github.com>
2025-03-24 19:19:30 +0300

    batch file as an executable

--- build.bat
+++ build.bat
@@ -1,13 +1,42 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCuDJGmW+1I1OltnQwqLPyuVFLwb4ebopNaUrUMUV6w2e4C7
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZks0
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTi
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATE9UwxIxpZTUSDMm/6Er0ZqP3z56qPq1sYVvA+cYG7
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDpaTQ+DMWqGIrof/eX+4f6UnmoUQMoqerPI1bqHI61Cp0LtYZ441DdXmc8CCQhLM0GXbBo9pWoC/mKEO9SZ8x/uWUGd7101Uitxn2aw
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 
-echo skibidi brainrot toilet goofy soo goated 
-
+echo skibidi brainrot toilet goofy soo goated
 echo hamburger
-
 echo ninja low taper fade
-
 echo bro got brainrot
-
 echo what the sigma
 
 pause 2 > nul
@@ -17,6 +46,7 @@ asm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- sonic1.asm,
 convsym sonic1.lst s1built.bin -input asm68k_lst -inopt "/localSign=@ /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+" -a
 fixheadr.exe s1built.bin
 
+cls
 echo see ya
 
 pause
