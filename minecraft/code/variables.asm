597942cae37fbf3881b7b90f423f61e99c3a515d
Naoto_ntp <sonimario101@gmail.com>
2025-03-25 00:35:23 -0400

    Created a Tile Scrolling Test

--- minecraft/code/variables.asm
+++ minecraft/code/variables.asm
@@ -11,11 +11,13 @@ mapCollBlocks	rs.b	MAP_HEIGHT*MAP_WIDTH	; collision layer layout data
 
 		rsset	$FFFF8000
 planeBuffer	rs.w	29*64			; foreground plane buffer
+planeBuffer_End	rs.b	0
+PLANE_BUFF_SIZE	equ	planeBuffer_End-planeBuffer
 
 camXPosFG	rs.w	1			; camera foreground layout x position
 camYposFG	rs.w	1			; camera foreground layout y position
 camXPosBG	rs.w	1			; camera background layout x position
 camYposBG	rs.w	1			; camera background layout y position
 
-		rsset	__RS&$FFFFFF
+		rsset	__rs&$FFFFFF
 mcRAM_End	rs.b	0
