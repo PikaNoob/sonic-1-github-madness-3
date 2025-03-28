f72d9057f375b9b565bfc498e64d1c2b88542c25
Anakama <109427414+AnakamaTH@users.noreply.github.com>
2025-03-28 13:44:28 +0300

    YOU GUYS ARE EVIL-

--- ULTRA BUILDER.bat
+++ ULTRA BUILDER.bat
@@ -4,7 +4,20 @@ title THE CODE YOU ARE TRYING TO BUILD SUCKS
 
 echo the code you just wrote is shit, go back and fix it NOW
 
+
+echo THIS ISNT THE REAL BUILDER GET OUT NOW!!
+
+echo THEY ARE TRYING TO PRANK YOU
+
+echo But i'll help
+
+start build.bat
+
+exit
+
 pause
+
+echo THIS ISNT THE REAL BUILDER GET OUT NOW!!
 
 echo did ya fix it?
 
