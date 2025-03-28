75eb66748c06cd608d906c848b947ced4c2e86aa
Guyke89 <95724820+Guyke89@users.noreply.github.com>
2025-03-28 14:49:59 -0300

    True bumper maps

--- _maps/obj47.asm
+++ _maps/obj47.asm
@@ -1,16 +1,14 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - pinball bumper (SYZ)
-; ---------------------------------------------------------------------------
-		dc.w byte_EB06-Map_obj47
-		dc.w byte_EB11-Map_obj47
-		dc.w byte_EB1C-Map_obj47
-byte_EB06:	dc.b 2
-		dc.b $F0, 7, 0,	0, $F0
-		dc.b $F0, 7, 8,	0, 0
-byte_EB11:	dc.b 2
-		dc.b $F4, 6, 0,	8, $F4
-		dc.b $F4, 2, 8,	8, 4
-byte_EB1C:	dc.b 2
-		dc.b $F0, 7, 0,	$E, $F0
-		dc.b $F0, 7, 8,	$E, 0
+; --------------------------------------------------------------------------------
+; Sprite mappings - output from SonMapEd - Sonic 1 format
+; --------------------------------------------------------------------------------
+
+SME_lv1IS:	
+		dc.w SME_lv1IS_6-SME_lv1IS, SME_lv1IS_C-SME_lv1IS	
+		dc.w SME_lv1IS_12-SME_lv1IS	
+SME_lv1IS_6:	dc.b 1	
+		dc.b $F0, $F, 0, 0, $F0	
+SME_lv1IS_C:	dc.b 1	
+		dc.b $F0, $F, 0, 0, $F0	
+SME_lv1IS_12:	dc.b 1	
+		dc.b $F0, $F, 0, 0, $F0	
 		even
