b5c69b60102b9dc9507ebefeb48d98ce098450ca
KatKuriN <69405557+KatKuriN@users.noreply.github.com>
2025-03-30 00:30:47 -0500

    imports

--- beebush/Objects/SPRPAT_PLAYER.asm
+++ beebush/Objects/SPRPAT_PLAYER.asm
@@ -1,20 +1,22 @@
-Map_596a:
-Map_596a_0: 	dc.w Map_596a_1C-Map_596a
-Map_596a_2: 	dc.w Map_596a_1D-Map_596a
-Map_596a_4: 	dc.w Map_596a_4B-Map_596a
-Map_596a_6: 	dc.w Map_596a_79-Map_596a
-Map_596a_8: 	dc.w Map_596a_A7-Map_596a
-Map_596a_A: 	dc.w Map_596a_D5-Map_596a
-Map_596a_C: 	dc.w Map_596a_103-Map_596a
-Map_596a_E: 	dc.w Map_596a_131-Map_596a
-Map_596a_10: 	dc.w Map_596a_15F-Map_596a
-Map_596a_12: 	dc.w Map_596a_165-Map_596a
-Map_596a_14: 	dc.w Map_596a_170-Map_596a
-Map_596a_16: 	dc.w Map_596a_180-Map_596a
-Map_596a_18: 	dc.w Map_596a_18B-Map_596a
-Map_596a_1A: 	dc.w Map_596a_1AA-Map_596a
-Map_596a_1C: 	dc.b $0
-Map_596a_1D: 	dc.b $9
+Map_5374:
+Map_5374_0: 	dc.w Map_5374_20-Map_5374
+Map_5374_2: 	dc.w Map_5374_21-Map_5374
+Map_5374_4: 	dc.w Map_5374_4F-Map_5374
+Map_5374_6: 	dc.w Map_5374_7D-Map_5374
+Map_5374_8: 	dc.w Map_5374_AB-Map_5374
+Map_5374_A: 	dc.w Map_5374_D9-Map_5374
+Map_5374_C: 	dc.w Map_5374_107-Map_5374
+Map_5374_E: 	dc.w Map_5374_135-Map_5374
+Map_5374_10: 	dc.w Map_5374_163-Map_5374
+Map_5374_12: 	dc.w Map_5374_169-Map_5374
+Map_5374_14: 	dc.w Map_5374_174-Map_5374
+Map_5374_16: 	dc.w Map_5374_184-Map_5374
+Map_5374_18: 	dc.w Map_5374_18F-Map_5374
+Map_5374_1A: 	dc.w Map_5374_1AE-Map_5374
+Map_5374_1C: 	dc.w Map_5374_1E1-Map_5374
+Map_5374_1E: 	dc.w Map_5374_214-Map_5374
+Map_5374_20: 	dc.b $0
+Map_5374_21: 	dc.b $9
 	dc.b $D1, $F, $1, $3, $EF
 	dc.b $F3, $9, $1, $13, $FB
 	dc.b $E4, $E, $1, $19, $B
@@ -24,7 +26,7 @@ Map_596a_1D: 	dc.b $9
 	dc.b $18, $6, $1, $6E, $ED
 	dc.b $0, $E, $9, $31, $4
 	dc.b $16, $A, $9, $3D, $C
-Map_596a_4B: 	dc.b $9
+Map_5374_4F: 	dc.b $9
 	dc.b $D0, $F, $0, $E3, $F1
 	dc.b $F3, $9, $1, $13, $FC
 	dc.b $E4, $E, $1, $19, $B
@@ -34,7 +36,7 @@ Map_596a_4B: 	dc.b $9
 	dc.b $18, $6, $1, $6E, $EE
 	dc.b $0, $A, $9, $56, $4
 	dc.b $18, $6, $9, $5F, $C
-Map_596a_79: 	dc.b $9
+Map_5374_7D: 	dc.b $9
 	dc.b $D0, $F, $0, $E3, $F3
 	dc.b $F3, $9, $1, $13, $FC
 	dc.b $E4, $E, $1, $19, $C
@@ -44,7 +46,7 @@ Map_596a_79: 	dc.b $9
 	dc.b $17, $6, $1, $5F, $EE
 	dc.b $0, $A, $9, $56, $5
 	dc.b $18, $6, $9, $5F, $D
-Map_596a_A7: 	dc.b $9
+Map_5374_AB: 	dc.b $9
 	dc.b $D0, $F, $0, $E3, $F5
 	dc.b $F3, $9, $1, $13, $FD
 	dc.b $E4, $E, $1, $19, $D
@@ -54,7 +56,7 @@ Map_596a_A7: 	dc.b $9
 	dc.b $17, $6, $1, $7D, $EF
 	dc.b $0, $A, $9, $56, $6
 	dc.b $18, $6, $9, $5F, $E
-Map_596a_D5: 	dc.b $9
+Map_5374_D9: 	dc.b $9
 	dc.b $D1, $F, $0, $F3, $F7
 	dc.b $F3, $9, $1, $13, $FE
 	dc.b $E4, $E, $1, $19, $E
@@ -64,7 +66,7 @@ Map_596a_D5: 	dc.b $9
 	dc.b $17, $6, $1, $3D, $E8
 	dc.b $0, $A, $9, $56, $7
 	dc.b $18, $6, $9, $5F, $F
-Map_596a_103: 	dc.b $9
+Map_5374_107: 	dc.b $9
 	dc.b $D1, $F, $0, $E3, $F3
 	dc.b $F3, $9, $1, $13, $FA
 	dc.b $E4, $E, $1, $19, $A
@@ -74,7 +76,7 @@ Map_596a_103: 	dc.b $9
 	dc.b $18, $6, $1, $5F, $EC
 	dc.b $10, $B, $9, $43, $B
 	dc.b $0, $9, $9, $4F, $4
-Map_596a_131: 	dc.b $9
+Map_5374_135: 	dc.b $9
 	dc.b $D0, $F, $0, $E3, $F2
 	dc.b $F3, $9, $1, $13, $FA
 	dc.b $E4, $E, $1, $19, $A
@@ -84,26 +86,26 @@ Map_596a_131: 	dc.b $9
 	dc.b $18, $6, $1, $6E, $EC
 	dc.b $0, $A, $9, $74, $4
 	dc.b $18, $6, $9, $7D, $C
-Map_596a_15F: 	dc.b $1
+Map_5374_163: 	dc.b $1
 	dc.b $F1, $F, $1, $83, $F1
-Map_596a_165: 	dc.b $2
+Map_5374_169: 	dc.b $2
 	dc.b $F2, $F, $1, $93, $F2
 	dc.b $4, $0, $1, $83, $ED
-Map_596a_170: 	dc.b $3
+Map_5374_174: 	dc.b $3
 	dc.b $F1, $F, $11, $83, $EF
 	dc.b $E8, $0, $1, $83, $EC
 	dc.b $F5, $0, $1, $83, $10
-Map_596a_180: 	dc.b $2
+Map_5374_184: 	dc.b $2
 	dc.b $EF, $F, $11, $93, $F2
 	dc.b $F2, $0, $1, $83, $E8
-Map_596a_18B: 	dc.b $6
+Map_5374_18F: 	dc.b $6
 	dc.b $F0, $F, $1, $A3, $E8
 	dc.b $10, $C, $1, $B3, $E8
 	dc.b $E8, $C, $1, $B7, $F0
 	dc.b $F0, $7, $1, $BB, $8
 	dc.b $10, $0, $1, $C3, $8
 	dc.b $E8, $0, $1, $C4, $10
-Map_596a_1AA: 	dc.b $A
+Map_5374_1AE: 	dc.b $A
 	dc.b $F0, $0, $1, $D4, $E8
 	dc.b $E8, $1, $1, $D1, $10
 	dc.b $E8, $E, $1, $C5, $F0
@@ -114,4 +116,24 @@ Map_596a_1AA: 	dc.b $A
 	dc.b $10, $0, $1, $C3, $8
 	dc.b $E8, $0, $1, $C4, $10
 	dc.b $E9, $0, $1, $D1, $F
+Map_5374_1E1: 	dc.b $A
+	dc.b $DC, $F, $1, $D6, $D8
+	dc.b $FC, $E, $1, $E6, $D8
+	dc.b $CC, $9, $1, $F2, $E0
+	dc.b $14, $F, $1, $F8, $F0
+	dc.b $D4, $F, $2, $8, $F8
+	dc.b $F4, $F, $2, $18, $F8
+	dc.b $14, $B, $2, $28, $10
+	dc.b $D4, $3, $2, $34, $18
+	dc.b $F4, $7, $2, $38, $18
+	dc.b $DC, $2, $2, $40, $20
+Map_5374_214: 	dc.b $8
+	dc.b $D8, $F, $2, $43, $D4
+	dc.b $F8, $A, $2, $53, $DC
+	dc.b $0, $7, $2, $5C, $F4
+	dc.b $8, $B, $2, $64, $4
+	dc.b $8, $5, $2, $70, $1C
+	dc.b $10, $0, $2, $74, $EC
+	dc.b $20, $4, $2, $75, $F4
+	dc.b $E9, $E, $1, $19, $EF
 	even
