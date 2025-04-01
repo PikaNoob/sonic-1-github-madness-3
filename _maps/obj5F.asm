4590a93d9fc28e6ef52f040b4728d0a01ba74275
Guyke89 <95724820+Guyke89@users.noreply.github.com>
2025-04-01 01:57:31 -0300

    last

--- _maps/obj5F.asm
+++ _maps/obj5F.asm
@@ -2,40 +2,40 @@
 ; Sprite mappings - output from SonMapEd - Sonic 1 format
 ; --------------------------------------------------------------------------------
 
-SME_1gJKr:	
-		dc.w SME_1gJKr_12-SME_1gJKr, SME_1gJKr_27-SME_1gJKr	
-		dc.w SME_1gJKr_3C-SME_1gJKr, SME_1gJKr_4C-SME_1gJKr	
-		dc.w SME_1gJKr_5C-SME_1gJKr, SME_1gJKr_6C-SME_1gJKr	
-		dc.w SME_1gJKr_72-SME_1gJKr, SME_1gJKr_78-SME_1gJKr	
-		dc.w SME_1gJKr_7E-SME_1gJKr	
-SME_1gJKr_12:	dc.b 4	
+SME_T0O61:	
+		dc.w SME_T0O61_12-SME_T0O61, SME_T0O61_27-SME_T0O61	
+		dc.w SME_T0O61_3C-SME_T0O61, SME_T0O61_4C-SME_T0O61	
+		dc.w SME_T0O61_5C-SME_T0O61, SME_T0O61_6C-SME_T0O61	
+		dc.w SME_T0O61_72-SME_T0O61, SME_T0O61_78-SME_T0O61	
+		dc.w SME_T0O61_7E-SME_T0O61	
+SME_T0O61_12:	dc.b 4	
 		dc.b $D1, $B, 0, 9, $F5	
 		dc.b $F1, $B, 0, $15, $F5	
 		dc.b 9, 0, 0, $21, $D	
 		dc.b $C1, 1, 0, 1, 0	
-SME_1gJKr_27:	dc.b 4	
+SME_T0O61_27:	dc.b 4	
 		dc.b $D1, $B, 0, 9, $F5	
 		dc.b $F1, $B, 0, $22, $F5	
 		dc.b 9, 0, 0, $2E, $D	
 		dc.b $C1, 1, 0, 1, 0	
-SME_1gJKr_3C:	dc.b 3	
+SME_T0O61_3C:	dc.b 3	
 		dc.b $D1, $B, 0, 9, $F5	
 		dc.b $F1, $B, 0, $2F, $F5	
 		dc.b $C1, 1, 0, 1, 0	
-SME_1gJKr_4C:	dc.b 3	
+SME_T0O61_4C:	dc.b 3	
 		dc.b $F1, $B, 0, $15, $F5	
 		dc.b 9, 0, 0, $21, $D	
 		dc.b $D1, $B, 0, $3B, $F5	
-SME_1gJKr_5C:	dc.b 3	
+SME_T0O61_5C:	dc.b 3	
 		dc.b $F1, $B, 0, $15, $F5	
 		dc.b 9, 0, 0, $21, $D	
 		dc.b $D2, $B, 0, $3B, $F5	
-SME_1gJKr_6C:	dc.b 1	
-		dc.b $C3, 1, 0, 3, $FD	
-SME_1gJKr_72:	dc.b 1	
-		dc.b $C3, 1, 0, 5, $FD	
-SME_1gJKr_78:	dc.b 1	
+SME_T0O61_6C:	dc.b 1	
+		dc.b $C3, 1, 0, 3, 1	
+SME_T0O61_72:	dc.b 1	
+		dc.b $C3, 1, 0, 5, 1	
+SME_T0O61_78:	dc.b 1	
 		dc.b $FC, 0, 0, 7, $FC	
-SME_1gJKr_7E:	dc.b 1	
+SME_T0O61_7E:	dc.b 1	
 		dc.b $FC, 0, 0, 8, $FC	
 		even
