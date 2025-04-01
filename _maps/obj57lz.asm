19fe000407b2cccef95e7a4a9c3bbec0e1efe823
Guyke89 <95724820+Guyke89@users.noreply.github.com>
2025-03-31 22:12:09 -0300

    spike ball maps

--- _maps/obj57lz.asm
+++ _maps/obj57lz.asm
@@ -1,13 +1,14 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - spiked ball	on a chain (LZ)
-; ---------------------------------------------------------------------------
-		dc.w byte_10964-Map_obj57a
-		dc.w byte_1096A-Map_obj57a
-		dc.w byte_10970-Map_obj57a
-byte_10964:	dc.b 1
-		dc.b $F8, 5, 0,	0, $F8
-byte_1096A:	dc.b 1
-		dc.b $F0, $F, 0, 4, $F0
-byte_10970:	dc.b 1
-		dc.b $F8, 5, 0,	$14, $F8
+; --------------------------------------------------------------------------------
+; Sprite mappings - output from SonMapEd - Sonic 1 format
+; --------------------------------------------------------------------------------
+
+SME_Diy6f:	
+		dc.w SME_Diy6f_6-SME_Diy6f, SME_Diy6f_C-SME_Diy6f	
+		dc.w SME_Diy6f_12-SME_Diy6f	
+SME_Diy6f_6:	dc.b 1	
+		dc.b $F8, 5, 0, 0, $F8	
+SME_Diy6f_C:	dc.b 1	
+		dc.b $F0, $F, $20, 4, $F0	
+SME_Diy6f_12:	dc.b 1	
+		dc.b $F8, 5, 0, $14, $F8	
 		even
