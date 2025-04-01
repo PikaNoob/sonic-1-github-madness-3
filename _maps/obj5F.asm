917654b81d0341c3afdda90572e5fbc90eee11be
Guyke89 <95724820+Guyke89@users.noreply.github.com>
2025-04-01 01:56:01 -0300

    last

--- _maps/obj5F.asm
+++ _maps/obj5F.asm
@@ -2,43 +2,40 @@
 ; Sprite mappings - output from SonMapEd - Sonic 1 format
 ; --------------------------------------------------------------------------------
 
-SME_WsgB2:	
-		dc.w SME_WsgB2_14-SME_WsgB2, SME_WsgB2_29-SME_WsgB2	
-		dc.w SME_WsgB2_39-SME_WsgB2, SME_WsgB2_49-SME_WsgB2	
-		dc.w SME_WsgB2_59-SME_WsgB2, SME_WsgB2_69-SME_WsgB2	
-		dc.w SME_WsgB2_6F-SME_WsgB2, SME_WsgB2_75-SME_WsgB2	
-		dc.w SME_WsgB2_7B-SME_WsgB2, SME_WsgB2_81-SME_WsgB2	
-SME_WsgB2_14:	dc.b 4	
-		dc.b $D1, $B, 0, 9, $F4	
-		dc.b $F1, $B, 0, $15, $F4	
-		dc.b 9, 0, 0, $21, $C	
-		dc.b $C1, 1, 0, 1, $FF	
-SME_WsgB2_29:	dc.b 3	
+SME_1gJKr:	
+		dc.w SME_1gJKr_12-SME_1gJKr, SME_1gJKr_27-SME_1gJKr	
+		dc.w SME_1gJKr_3C-SME_1gJKr, SME_1gJKr_4C-SME_1gJKr	
+		dc.w SME_1gJKr_5C-SME_1gJKr, SME_1gJKr_6C-SME_1gJKr	
+		dc.w SME_1gJKr_72-SME_1gJKr, SME_1gJKr_78-SME_1gJKr	
+		dc.w SME_1gJKr_7E-SME_1gJKr	
+SME_1gJKr_12:	dc.b 4	
 		dc.b $D1, $B, 0, 9, $F5	
-		dc.b $F1, $F, 0, $22, $F5	
+		dc.b $F1, $B, 0, $15, $F5	
+		dc.b 9, 0, 0, $21, $D	
 		dc.b $C1, 1, 0, 1, 0	
-SME_WsgB2_39:	dc.b 3	
-		dc.b $D1, $B, 0, 9, $F6	
-		dc.b $F1, $B, 0, $32, $F6	
-		dc.b $C1, 1, 0, 1, 1	
-SME_WsgB2_49:	dc.b 3	
-		dc.b $F1, $B, 0, $15, $F4	
-		dc.b 9, 0, 0, $21, $C	
-		dc.b $D1, $B, 0, $3E, $F4	
-SME_WsgB2_59:	dc.b 3	
-		dc.b $F1, $B, 0, $15, $F4	
-		dc.b 9, 0, 0, $21, $C	
-		dc.b $D2, $B, 0, $3E, $F4	
-SME_WsgB2_69:	dc.b 1	
-		dc.b $C7, 1, 0, 3, $FC	
-SME_WsgB2_6F:	dc.b 1	
-		dc.b $C7, 1, 0, 5, $FC	
-SME_WsgB2_75:	dc.b 1	
+SME_1gJKr_27:	dc.b 4	
+		dc.b $D1, $B, 0, 9, $F5	
+		dc.b $F1, $B, 0, $22, $F5	
+		dc.b 9, 0, 0, $2E, $D	
+		dc.b $C1, 1, 0, 1, 0	
+SME_1gJKr_3C:	dc.b 3	
+		dc.b $D1, $B, 0, 9, $F5	
+		dc.b $F1, $B, 0, $2F, $F5	
+		dc.b $C1, 1, 0, 1, 0	
+SME_1gJKr_4C:	dc.b 3	
+		dc.b $F1, $B, 0, $15, $F5	
+		dc.b 9, 0, 0, $21, $D	
+		dc.b $D1, $B, 0, $3B, $F5	
+SME_1gJKr_5C:	dc.b 3	
+		dc.b $F1, $B, 0, $15, $F5	
+		dc.b 9, 0, 0, $21, $D	
+		dc.b $D2, $B, 0, $3B, $F5	
+SME_1gJKr_6C:	dc.b 1	
+		dc.b $C3, 1, 0, 3, $FD	
+SME_1gJKr_72:	dc.b 1	
+		dc.b $C3, 1, 0, 5, $FD	
+SME_1gJKr_78:	dc.b 1	
 		dc.b $FC, 0, 0, 7, $FC	
-SME_WsgB2_7B:	dc.b 1	
+SME_1gJKr_7E:	dc.b 1	
 		dc.b $FC, 0, 0, 8, $FC	
-SME_WsgB2_81:	dc.b 3	
-		dc.b $FC, $F, 0, $4A, $FC	
-		dc.b $1C, 3, 0, $5A, $FC	
-		dc.b $3C, 2, 0, $5E, $FC	
 		even
