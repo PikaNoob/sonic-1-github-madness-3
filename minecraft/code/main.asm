85b71381deae9d45c6cbc8991ce97245892c7e24
Naoto_ntp <sonimario101@gmail.com>
2025-04-01 17:34:13 -0400

    Fixed the background loading bug

--- minecraft/code/main.asm
+++ minecraft/code/main.asm
@@ -317,8 +317,8 @@ MC_ClearScreen:
 MC_LoadBackground:
 	dma68k	MC_BGArt,vramBackground,MC_BGArt_End-MC_BGArt,VRAM	; load in the background art
 
-	vdpCmd	move.l, VRAM_PLANE_B, VRAM, WRITE, (a6)		; Set up VDP to write data to the plane B nametable location
 		lea	MC_BGMap,a0
+		lea	mcRAM,a1
 		move.w	#(32*64)-1,d7				; Set loop count to plane size - 1
 
 .loop:
@@ -328,8 +328,11 @@ MC_LoadBackground:
 		ori.w	#(3<<13),d0
 
 .empty:
-		move.w	d0,-4(a6)
+		move.w	d0,(a1)+
 		dbf	d7,.loop
+
+	dma68k	mcRAM,VRAM_PLANE_B,32*64*2,VRAM
+	
 		rts						; return
 ; ---------------------------------------------------------------------------
 
