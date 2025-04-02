8e2cbed909a558e1689718d855c7f568cdbe5475
KatKuriN <69405557+KatKuriN@users.noreply.github.com>
2025-04-01 22:14:58 -0500

    god

---
+++ _maps/obj7E.asm
@@ -0,0 +1,84 @@
+Map_884a:
+Map_884a_0: 	dc.w Map_884a_12-Map_884a
+Map_884a_2: 	dc.w Map_884a_54-Map_884a
+Map_884a_4: 	dc.w Map_884a_73-Map_884a
+Map_884a_6: 	dc.w Map_884a_97-Map_884a
+Map_884a_8: 	dc.w Map_884a_98-Map_884a
+Map_884a_A: 	dc.w Map_884a_AD-Map_884a
+Map_884a_C: 	dc.w Map_884a_C2-Map_884a
+Map_884a_E: 	dc.w Map_884a_D2-Map_884a
+Map_884a_10: 	dc.w Map_884a_10F-Map_884a
+Map_884a_12: 	dc.b $D
+	dc.b $F8, $5, $0, $8, $90
+	dc.b $F8, $5, $0, $1C, $A0
+	dc.b $F8, $5, $0, $0, $B0
+	dc.b $F8, $5, $0, $32, $C0
+	dc.b $F8, $5, $0, $3E, $D0
+	dc.b $F8, $5, $0, $10, $F0
+	dc.b $F8, $5, $0, $2A, $0
+	dc.b $F8, $5, $0, $10, $10
+	dc.b $F8, $5, $0, $3A, $20
+	dc.b $F8, $5, $0, $0, $30
+	dc.b $F8, $5, $0, $26, $40
+	dc.b $F8, $5, $0, $C, $50
+	dc.b $F8, $5, $0, $3E, $60
+Map_884a_54: 	dc.b $6
+	dc.b $F8, $D, $1, $4A, $B0
+	dc.b $F8, $1, $1, $62, $D0
+	dc.b $F8, $9, $1, $64, $18
+	dc.b $F8, $D, $1, $6A, $30
+	dc.b $F7, $4, $0, $6E, $CD
+	dc.b $FF, $4, $18, $6E, $CD
+Map_884a_73: 	dc.b $7
+	dc.b $F8, $D, $1, $52, $B0
+	dc.b $F8, $D, $0, $66, $D9
+	dc.b $F8, $1, $1, $4A, $F9
+	dc.b $F7, $4, $0, $6E, $F6
+	dc.b $FF, $4, $18, $6E, $F6
+	dc.b $F8, $D, $FF, $F8, $28
+	dc.b $F8, $1, $1, $70, $48
+Map_884a_97: 	dc.b $0
+Map_884a_98: 	dc.b $4
+	dc.b $F8, $D, $FF, $D1, $B0
+	dc.b $F8, $D, $FF, $D9, $D0
+	dc.b $F8, $1, $FF, $E1, $F0
+	dc.b $F8, $6, $1F, $E3, $40
+Map_884a_AD: 	dc.b $4
+	dc.b $F8, $D, $FF, $D1, $B0
+	dc.b $F8, $D, $FF, $D9, $D0
+	dc.b $F8, $1, $FF, $E1, $F0
+	dc.b $F8, $6, $1F, $E9, $40
+Map_884a_C2: 	dc.b $3
+	dc.b $F8, $D, $FF, $D1, $B0
+	dc.b $F8, $D, $FF, $D9, $D0
+	dc.b $F8, $1, $FF, $E1, $F0
+Map_884a_D2: 	dc.b $C
+	dc.b $F8, $5, $0, $3E, $9C
+	dc.b $F8, $5, $0, $36, $AC
+	dc.b $F8, $5, $0, $10, $BC
+	dc.b $F8, $5, $0, $8, $CC
+	dc.b $F8, $1, $0, $20, $DC
+	dc.b $F8, $5, $0, $0, $E4
+	dc.b $F8, $5, $0, $26, $F4
+	dc.b $F8, $5, $0, $3E, $14
+	dc.b $F8, $5, $0, $42, $24
+	dc.b $F8, $5, $0, $0, $34
+	dc.b $F8, $5, $0, $18, $44
+	dc.b $F8, $5, $0, $10, $54
+Map_884a_10F: 	dc.b $F
+	dc.b $F8, $5, $0, $3E, $88
+	dc.b $F8, $5, $0, $32, $98
+	dc.b $F8, $5, $0, $2E, $A8
+	dc.b $F8, $1, $0, $20, $B8
+	dc.b $F8, $5, $0, $8, $C0
+	dc.b $F8, $5, $0, $18, $D8
+	dc.b $F8, $5, $0, $32, $E8
+	dc.b $F8, $5, $0, $42, $F8
+	dc.b $F8, $5, $0, $42, $10
+	dc.b $F8, $5, $0, $1C, $20
+	dc.b $F8, $5, $0, $10, $30
+	dc.b $F8, $5, $0, $2A, $40
+	dc.b $F8, $5, $0, $0, $58
+	dc.b $F8, $5, $0, $26, $68
+	dc.b $F8, $5, $0, $26, $78
+	even
