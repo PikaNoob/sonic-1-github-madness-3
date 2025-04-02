2a4644cb4b5dee8903d8ce78e394c8c3b5e68fa3
conic the cat <yazn0996@gmail.com>
2025-04-03 01:36:09 +0300

    Update conic.asm

--- _maps/conic.asm
+++ _maps/conic.asm
@@ -1,63 +1,63 @@
-Map_2cc1:
-Map_2cc1_0: 	dc.w Map_2cc1_1A-Map_2cc1
-Map_2cc1_2: 	dc.w Map_2cc1_2F-Map_2cc1
-Map_2cc1_4: 	dc.w Map_2cc1_44-Map_2cc1
-Map_2cc1_6: 	dc.w Map_2cc1_59-Map_2cc1
-Map_2cc1_8: 	dc.w Map_2cc1_69-Map_2cc1
-Map_2cc1_A: 	dc.w Map_2cc1_79-Map_2cc1
-Map_2cc1_C: 	dc.w Map_2cc1_89-Map_2cc1
-Map_2cc1_E: 	dc.w Map_2cc1_A3-Map_2cc1
-Map_2cc1_10: 	dc.w Map_2cc1_B3-Map_2cc1
-Map_2cc1_12: 	dc.w Map_2cc1_B9-Map_2cc1
-Map_2cc1_14: 	dc.w Map_2cc1_BF-Map_2cc1
-Map_2cc1_16: 	dc.w Map_2cc1_C0-Map_2cc1
-Map_2cc1_18: 	dc.w Map_2cc1_CB-Map_2cc1
-Map_2cc1_1A: 	dc.b $4
+Map_5554:
+Map_5554_0: 	dc.w Map_5554_1A-Map_5554
+Map_5554_2: 	dc.w Map_5554_2F-Map_5554
+Map_5554_4: 	dc.w Map_5554_44-Map_5554
+Map_5554_6: 	dc.w Map_5554_59-Map_5554
+Map_5554_8: 	dc.w Map_5554_69-Map_5554
+Map_5554_A: 	dc.w Map_5554_79-Map_5554
+Map_5554_C: 	dc.w Map_5554_89-Map_5554
+Map_5554_E: 	dc.w Map_5554_A3-Map_5554
+Map_5554_10: 	dc.w Map_5554_B3-Map_5554
+Map_5554_12: 	dc.w Map_5554_B9-Map_5554
+Map_5554_14: 	dc.w Map_5554_BF-Map_5554
+Map_5554_16: 	dc.w Map_5554_C0-Map_5554
+Map_5554_18: 	dc.w Map_5554_CB-Map_5554
+Map_5554_1A: 	dc.b $4
 	dc.b $F8, $7, $40, $0, $F0
 	dc.b $F8, $F, $40, $8, $8
 	dc.b $0, $A, $40, $18, $D8
 	dc.b $0, $2, $40, $21, $0
-Map_2cc1_2F: 	dc.b $4
+Map_5554_2F: 	dc.b $4
 	dc.b $D8, $3, $40, $24, $F8
 	dc.b $E0, $6, $40, $28, $E8
 	dc.b $E0, $3, $40, $2E, $0
 	dc.b $E8, $0, $40, $32, $E0
-Map_2cc1_44: 	dc.b $4
+Map_5554_44: 	dc.b $4
 	dc.b $D8, $3, $40, $24, $F8
 	dc.b $E0, $6, $40, $28, $E8
 	dc.b $E0, $3, $40, $2E, $0
 	dc.b $E8, $0, $40, $32, $E0
-Map_2cc1_59: 	dc.b $3
+Map_5554_59: 	dc.b $3
 	dc.b $E0, $E, $40, $33, $E7
 	dc.b $D8, $0, $40, $3F, $FF
 	dc.b $F8, $4, $40, $40, $FF
-Map_2cc1_69: 	dc.b $3
+Map_5554_69: 	dc.b $3
 	dc.b $E0, $E, $40, $42, $E7
 	dc.b $D8, $0, $40, $4E, $FF
 	dc.b $F8, $4, $40, $4F, $FF
-Map_2cc1_79: 	dc.b $3
+Map_5554_79: 	dc.b $3
 	dc.b $E0, $E, $40, $51, $E7
 	dc.b $D8, $0, $40, $5D, $FF
 	dc.b $F8, $4, $40, $5E, $FF
-Map_2cc1_89: 	dc.b $5
+Map_5554_89: 	dc.b $5
 	dc.b $D8, $3, $40, $24, $F8
 	dc.b $E0, $6, $40, $28, $E8
 	dc.b $E0, $3, $40, $2E, $0
 	dc.b $E8, $0, $40, $32, $E0
 	dc.b $D8, $4, $0, $60, $0
-Map_2cc1_A3: 	dc.b $3
+Map_5554_A3: 	dc.b $3
 	dc.b $E0, $E, $40, $51, $E7
 	dc.b $D8, $0, $40, $5D, $FF
 	dc.b $F8, $4, $40, $5E, $FF
-Map_2cc1_B3: 	dc.b $1
-	dc.b $4, $5, $0, $62, $22
-Map_2cc1_B9: 	dc.b $1
-	dc.b $4, $5, $0, $66, $22
-Map_2cc1_BF: 	dc.b $0
-Map_2cc1_C0: 	dc.b $2
-	dc.b $0, $8, $0, $6A, $22
-	dc.b $8, $8, $10, $6A, $22
-Map_2cc1_CB: 	dc.b $2
-	dc.b $F8, $B, $0, $6A, $22
-	dc.b $0, $1, $0, $6A, $3A
+Map_5554_B3: 	dc.b $1
+	dc.b $4, $5, $8, $62, $CE
+Map_5554_B9: 	dc.b $1
+	dc.b $4, $5, $8, $66, $CE
+Map_5554_BF: 	dc.b $0
+Map_5554_C0: 	dc.b $2
+	dc.b $0, $8, $9, $2A, $C6
+	dc.b $8, $8, $19, $2A, $C6
+Map_5554_CB: 	dc.b $2
+	dc.b $F8, $B, $9, $2D, $C6
+	dc.b $0, $1, $9, $39, $BE
 	even
