87ff00a2d47c5fd9c959130be1f43ea2b7c8d9fe
conic the cat <yazn0996@gmail.com>
2025-04-09 17:04:28 +0300

    set up mercury results assets

---
+++ _maps/mercurywin.asm
@@ -0,0 +1,24 @@
+Map_1cb1:
+Map_1cb1_0: 	dc.w Map_1cb1_6-Map_1cb1
+Map_1cb1_2: 	dc.w Map_1cb1_25-Map_1cb1
+Map_1cb1_4: 	dc.w Map_1cb1_3F-Map_1cb1
+Map_1cb1_6: 	dc.b $6
+	dc.b $D2, $B, $40, $0, $F1
+	dc.b $F2, $B, $40, $C, $F0
+	dc.b $FA, $0, $40, $18, $8
+	dc.b $12, $B, $40, $19, $F2
+	dc.b $2A, $0, $40, $25, $A
+	dc.b $2F, $0, $40, $26, $F0
+Map_1cb1_25: 	dc.b $5
+	dc.b $CC, $4, $40, $27, $F5
+	dc.b $D4, $B, $40, $29, $F1
+	dc.b $F4, $B, $40, $35, $F1
+	dc.b $14, $F, $40, $41, $EF
+	dc.b $FA, $0, $40, $18, $9
+Map_1cb1_3F: 	dc.b $5
+	dc.b $CB, $4, $40, $51, $F6
+	dc.b $D3, $B, $40, $53, $F3
+	dc.b $F3, $B, $40, $5F, $F4
+	dc.b $F9, $0, $40, $6B, $EC
+	dc.b $13, $F, $40, $6C, $EF
+	even
