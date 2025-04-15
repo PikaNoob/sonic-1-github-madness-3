b595cdc3858c7621d1df6d4e1b4b194907519223
conic the cat <yazn0996@gmail.com>
2025-04-15 22:34:34 +0300

    Revert "TRY to add teto shatter"
    This reverts commit 186c8a50ec4b8767db6e2c9e1f4e10ec4ca9b395.

--- _maps/Tetoboss.asm
+++ _maps/Tetoboss.asm
@@ -1,45 +1,28 @@
-Map_cba0:
-Map_cba0_0: 	dc.w Map_cba0_E-Map_cba0
-Map_cba0_2: 	dc.w Map_cba0_1E-Map_cba0
-Map_cba0_4: 	dc.w Map_cba0_3D-Map_cba0
-Map_cba0_6: 	dc.w Map_cba0_4D-Map_cba0
-Map_cba0_8: 	dc.w Map_cba0_6C-Map_cba0
-Map_cba0_A: 	dc.w Map_cba0_72-Map_cba0
-Map_cba0_C: 	dc.w Map_cba0_A5-Map_cba0
-Map_cba0_E: 	dc.b $3
+Map_f09a:
+Map_f09a_0: 	dc.w Map_f09a_8-Map_f09a
+Map_f09a_2: 	dc.w Map_f09a_18-Map_f09a
+Map_f09a_4: 	dc.w Map_f09a_37-Map_f09a
+Map_f09a_6: 	dc.w Map_f09a_47-Map_f09a
+Map_f09a_8: 	dc.b $3
 	dc.b $E5, $F, $60, $2B, $F0
 	dc.b $5, $E, $60, $3B, $F0
 	dc.b $DD, $0, $60, $47, $F8
-Map_cba0_1E: 	dc.b $6
+Map_f09a_18: 	dc.b $6
 	dc.b $EC, $E, $60, $0, $E4
 	dc.b $4, $E, $60, $C, $EC
 	dc.b $E4, $8, $60, $18, $F4
 	dc.b $1C, $0, $60, $1B, $F4
 	dc.b $EC, $A, $60, $1C, $4
 	dc.b $4, $6, $60, $25, $C
-Map_cba0_3D: 	dc.b $3
+Map_f09a_37: 	dc.b $3
 	dc.b $E5, $F, $68, $2B, $F0
 	dc.b $5, $E, $68, $3B, $F0
 	dc.b $DD, $0, $68, $47, $0
-Map_cba0_4D: 	dc.b $6
+Map_f09a_47: 	dc.b $6
 	dc.b $ED, $E, $60, $48, $E4
 	dc.b $5, $E, $60, $54, $EC
 	dc.b $E5, $8, $60, $60, $F4
 	dc.b $DD, $0, $60, $63, $FC
 	dc.b $ED, $A, $60, $64, $4
 	dc.b $5, $2, $60, $6D, $C
-Map_cba0_6C: 	dc.b $1
-	dc.b $F4, $A, $60, $70, $F4
-Map_cba0_72: 	dc.b $A
-	dc.b $DC, $E, $60, $79, $DC
-	dc.b $14, $9, $60, $85, $DC
-	dc.b $F4, $9, $60, $8B, $E4
-	dc.b $C, $8, $60, $91, $E4
-	dc.b $4, $C, $60, $94, $EC
-	dc.b $DC, $7, $60, $98, $4
-	dc.b $FC, $8, $60, $A0, $4
-	dc.b $C, $A, $60, $A3, $4
-	dc.b $4, $8, $60, $AC, $C
-	dc.b $E4, $6, $60, $AF, $14
-Map_cba0_A5: 	dc.b $0
 	even
