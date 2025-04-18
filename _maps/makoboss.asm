6a09f609b9e593b8c803e92b82454ba832db9c1b
conic the cat <yazn0996@gmail.com>
2025-04-18 11:39:45 +0300

    explosions ka ploww
    the metal slug one would be used for dx ig
    ignoring my amazing typo

--- _maps/makoboss.asm
+++ _maps/makoboss.asm
@@ -1,27 +1,47 @@
-Map_2ba6:
-Map_2ba6_0: 	dc.w Map_2ba6_8-Map_2ba6
-Map_2ba6_2: 	dc.w Map_2ba6_1D-Map_2ba6
-Map_2ba6_4: 	dc.w Map_2ba6_37-Map_2ba6
-Map_2ba6_6: 	dc.w Map_2ba6_4C-Map_2ba6
-Map_2ba6_8: 	dc.b $4
+Map_dbdd:
+Map_dbdd_0: 	dc.w Map_dbdd_E-Map_dbdd
+Map_dbdd_2: 	dc.w Map_dbdd_23-Map_dbdd
+Map_dbdd_4: 	dc.w Map_dbdd_3D-Map_dbdd
+Map_dbdd_6: 	dc.w Map_dbdd_52-Map_dbdd
+Map_dbdd_8: 	dc.w Map_dbdd_67-Map_dbdd
+Map_dbdd_A: 	dc.w Map_dbdd_77-Map_dbdd
+Map_dbdd_C: 	dc.w Map_dbdd_AF-Map_dbdd
+Map_dbdd_E: 	dc.b $4
 	dc.b $E4, $F, $60, $24, $EC
 	dc.b $4, $D, $60, $34, $EC
 	dc.b $14, $4, $60, $3C, $F4
 	dc.b $E4, $3, $60, $3E, $C
-Map_2ba6_1D: 	dc.b $5
+Map_dbdd_23: 	dc.b $5
 	dc.b $E4, $F, $60, $0, $F0
 	dc.b $E4, $1, $60, $10, $10
 	dc.b $EC, $3, $60, $12, $E8
 	dc.b $FC, $1, $60, $16, $10
 	dc.b $4, $E, $60, $18, $F0
-Map_2ba6_37: 	dc.b $4
+Map_dbdd_3D: 	dc.b $4
 	dc.b $E4, $F, $68, $24, $F4
 	dc.b $4, $D, $68, $34, $F4
 	dc.b $14, $4, $68, $3C, $FC
 	dc.b $E4, $3, $68, $3E, $EC
-Map_2ba6_4C: 	dc.b $4
+Map_dbdd_52: 	dc.b $4
 	dc.b $E4, $F, $60, $42, $F0
 	dc.b $EC, $2, $60, $52, $10
 	dc.b $FC, $1, $60, $55, $E8
 	dc.b $4, $E, $60, $57, $F0
+Map_dbdd_67: 	dc.b $3
+	dc.b $F4, $F, $0, $63, $E8
+	dc.b $EC, $C, $0, $73, $F0
+	dc.b $F4, $7, $0, $77, $8
+Map_dbdd_77: 	dc.b $B
+	dc.b $D4, $F, $0, $7F, $F8
+	dc.b $DC, $7, $0, $8F, $18
+	dc.b $E4, $B, $0, $97, $E0
+	dc.b $F4, $3, $0, $A3, $D8
+	dc.b $F4, $F, $0, $A7, $F8
+	dc.b $FC, $7, $0, $B7, $18
+	dc.b $4, $B, $0, $BF, $E0
+	dc.b $14, $0, $0, $CB, $D8
+	dc.b $14, $E, $0, $CC, $F8
+	dc.b $1C, $0, $0, $D8, $18
+	dc.b $24, $4, $0, $D9, $E8
+Map_dbdd_AF: 	dc.b $0
 	even
